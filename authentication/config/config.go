b897495379408ea1d771dbad0e2f7aed133e6a4c
demola234 <kolawoleoluwasegun567@gmail.com>
2025-04-08 08:12:39 +0100

    feature(authentication): enhance configuration loading and update Makefile for environment variables

--- authentication/config/config.go
+++ authentication/config/config.go
@@ -12,13 +12,29 @@ type Config struct {
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
+	viper.SetConfigName(".env")
 
+	// Set the path to look for the configuration file
+	viper.AddConfigPath(path)
+
+	// Set the file type to .env
+	viper.SetDefault("DB_DRIVER", "postgres")
+	viper.SetDefault("DB_SOURCE", "postgres://root:secret@localhost:5433/defi?sslmode=disable")
+	viper.SetDefault("GRPC_SERVER_ADDRESS", ":50051")
+	viper.SetDefault("TOKEN_SYMMETRIC_KEY", "12345678901234567890123456789012")
 	viper.AutomaticEnv()
 
+	// Set the type of the configuration file
+	viper.SetConfigType("env")
+
+	// Read the configuration file
 	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	// Unmarshal the configuration into the Config struct
+	err = viper.Unmarshal(&config)
 	if err != nil {
 		return
 	}
